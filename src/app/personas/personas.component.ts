c8ef75653b5439d8bb6a380b50ccd43e8a312614
Davidhzk2 <guffyherrera150@gmail.com>
2022-04-16 22:42:19 -0500

    Modificando librerias de bootstras y agregando stylos

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,10 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'app-personas',
-    template:`
-    <h1>Listado de personas</h1>
-    <app-persona></app-persona>
-    <app-persona></app-persona>`,
+    templateUrl:'./personas.component.html',
+    // styleUrls:['./personas.component.css'],
+    styles:['h1{color:aqua;}']
 })
 
 export class PersonasComponent{}
