4ff6a265faff9f1df57d2889b3a971cb8df6ad1c
Davidhzk2 <guffyherrera150@gmail.com>
2022-04-21 21:26:32 -0500

    Event Binding: Manjer eventos con Angular en elementos HTML

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,10 +3,21 @@ import {Component} from '@angular/core';
 @Component({
     selector:'app-personas',
     templateUrl:'./personas.component.html',
-    // styleUrls:['./personas.component.css'],
-    styles:['h1{color:aqua;}']
+    styleUrls:['./personas.component.css']
+    // styles:['h1{color:aqua;}']
 })
 
 export class PersonasComponent{
     desabilitar = false;
+
+    mensaje = 'No se ha agreado ninguna persona ';
+    titulo ='';
+
+    agregarPersona(){
+        this.mensaje = 'Persona Agregada!';
+    }
+
+    modificarTitulo(event: Event){
+        this.titulo =(<HTMLInputElement>event.target).value;
+    }
 }
